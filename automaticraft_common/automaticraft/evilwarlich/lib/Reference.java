e55b4585a8c497437c478bc10ac8a1e65dfc1c8c
Xares <ndc@dr.com>
2013-05-20 10:50:07 +0200

    part of the skeleton of the mod, a reference file for refering to

---
+++ automaticraft_common/automaticraft/evilwarlich/lib/Reference.java
@@ -0,0 +1,9 @@
+package automaticraft.evilwarlich.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "AC";
+    public static final String MOD_NAME = "AutomatiCraft";
+    public static final String VERSION = "0.0.1";
+
+}
